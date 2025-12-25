86a6fe78567733ff172b5c0f32f26038160276e5
joseph0x45 <zozozozeph@gmail.com>
2025-12-25 15:16:06 +0000

    feat: delete and list all auth tokens

--- cli/auth_tokens.go
+++ cli/auth_tokens.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/joseph0x45/nidavellir/db"
@@ -12,6 +13,12 @@ import (
 func handleAuthTokensCmds(config *Config, db *db.Conn) {
 	if config.Create {
 		createAuthToken(config.Label, db)
+	} else if config.List {
+		listTokens(db)
+	} else if config.Delete {
+		deleteToken(config.Label, db)
+	} else {
+		printUsage()
 	}
 }
 
@@ -40,4 +47,41 @@ func createAuthToken(label string, db *db.Conn) {
 		return
 	}
 	log.Println("New auth token created:", newAuthToken.Token)
+}
+
+func listTokens(db *db.Conn) {
+	tokens, err := db.GetAllTokens()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	maxLabelLen := len("LABEL")
+	for _, t := range tokens {
+		if len(t.Label) > maxLabelLen {
+			maxLabelLen = len(t.Label)
+		}
+	}
+
+	fmt.Printf("%-*s  %s\n", maxLabelLen, "LABEL", "TOKEN")
+	fmt.Printf("%s  %s\n",
+		strings.Repeat("-", maxLabelLen),
+		strings.Repeat("-", 32),
+	)
+
+	for _, t := range tokens {
+		fmt.Printf("%-*s  %s\n", maxLabelLen, t.Label, t.Token)
+	}
+}
+
+func deleteToken(label string, db *db.Conn) {
+	if label == "" {
+		fmt.Println("flag 'label' is required when deleting a token")
+		printUsage()
+		return
+	}
+	if err := db.DeleteToken(label); err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println("Token", label, "deleted")
 }
